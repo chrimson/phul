b577f969973e4fd8622f083f20fa7b85236d1413
chrimson <chrimson@localhost.localdomain>
2020-02-14 09:57:55 -0500

    Call http

--- ph.py
+++ ph.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python -B
 import re
 import requests
+
+# from urllib3.exceptions import InsecureRequestWarning
+# requests.packages.urllib3.disable_warnings(category = InsecureRequestWarning)
+
+response = requests.get('https://www.google.com',
+                        verify = False,
+                        headers = {'Content-Type':'application/json'})
+
+f = re.findall('google', response.text)
+
+print
+print(f)

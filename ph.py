cf8004c87b6b77596eeb49b17cc1d85d18903415
chrimson <chrimson@localhost.localdomain>
2020-02-14 13:40:52 -0500

    Include different schema

--- ph.py
+++ ph.py
@@ -2,9 +2,15 @@
 import json
 import requests
 
-response = requests.get('https://www.powerball.com/api/v1/numbers/powerball?_format=json&min=1990-01-14%2000:00:00&max=2014-02-14%2000:00:00',
-                        verify = False,
-                        headers = {'Content-Type':'application/json'})
+payload = '{pageSize: 20, startDate: "10/01/2019", endDate: "11/14/2019", pageNumber: 1 }'
+
+#response = requests.get('https://www.powerball.com/api/v1/numbers/powerball?_format=json&min=1990-01-14%2000:00:00&max=2014-02-14%2000:00:00',
+response = requests.post('https://www.megamillions.com/cmspages/utilservice.asmx/GetDrawingPagingData',
+                         verify = False,
+#                        headers = {'Content-Type':'application/json'})
+                         data = payload,
+                         headers = {'content-type':'application/json; charset=UTF-8',
+                                    'referer':'https://www.megamillions.com/Winning-Numbers/Previous-Drawings.aspx?pageNumber=1&pageSize=20&startDate=10/01/2019&endDate=11/14/2019'})
 print
 
 j = json.loads(response.text)

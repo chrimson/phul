3aba260e44c6ae37bb33cc4dc38bc3022fb72379
chrimson <chrimson@localhost.localdomain>
2020-02-14 14:39:00 -0500

    Start dates

--- ph.py
+++ ph.py
@@ -2,16 +2,18 @@
 import json
 import requests
 
-payload = '{pageSize: 20, startDate: "10/01/2019", endDate: "11/14/2019", pageNumber: 1 }'
+payload = '{pageSize: 400, startDate: "10/01/2008", endDate: "02/04/2010", pageNumber: 1 }'
 
-#response = requests.get('https://www.powerball.com/api/v1/numbers/powerball?_format=json&min=1990-01-14%2000:00:00&max=2014-02-14%2000:00:00',
-response = requests.post('https://www.megamillions.com/cmspages/utilservice.asmx/GetDrawingPagingData',
+response = requests.get('https://www.powerball.com/api/v1/numbers/powerball?_format=json&min=1996-06-01%2000:00:00&max=1997-11-03%2000:00:00',
+#response = requests.post('https://www.megamillions.com/cmspages/utilservice.asmx/GetDrawingPagingData',
                          verify = False,
-#                        headers = {'Content-Type':'application/json'})
-                         data = payload,
-                         headers = {'content-type':'application/json; charset=UTF-8',
-                                    'referer':'https://www.megamillions.com/Winning-Numbers/Previous-Drawings.aspx?pageNumber=1&pageSize=20&startDate=10/01/2019&endDate=11/14/2019'})
+                         headers = {'Content-Type':'application/json'},
+                         data = payload)
 print
 
 j = json.loads(response.text)
 print(json.dumps(j, indent=4))
+
+#data = j['d']
+#j2 = json.loads(data)
+#print(json.dumps(j2, indent=4))

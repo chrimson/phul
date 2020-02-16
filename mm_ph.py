7e89566d544047f4f3c52c1b97e43e2aa895ea1f
chrimson <chrimson@localhost.localdomain>
2020-02-16 01:52:48 -0500

    Interface db

--- mm_ph.py
+++ mm_ph.py
@@ -2,18 +2,13 @@
 import json
 import requests
 
-payload = '{pageSize: 403, startDate: "02/04/2010", endDate: "03/04/2020", pageNumber: 3 }'
+payload = '{pageSize: 403, startDate: "01/21/2010", endDate: "03/04/2020", pageNumber: 3 }'
 
-#response = requests.get('https://www.powerball.com/api/v1/numbers/powerball?_format=json&min=1996-06-01%2000:00:00&max=1997-11-03%2000:00:00',
-response = requests.post('https://www.megamillions.com/cmspages/utilservice.asmx/GetDrawingPagingData',
+response = requests.post('https://www.game.com/cmspages/utilservice.asmx/GetDrawingPagingData',
                          verify = False,
                          headers = {'Content-Type':'application/json'},
                          data = payload)
-print
 
-j = json.loads(response.text)
-#print(json.dumps(j, indent=4))
-
-data = j['d']
-j2 = json.loads(data)
-print(json.dumps(j2, indent=4))
+data = response.json()
+jstr = data['d']
+drawings = json.loads(jstr)
--- pb_ph.py
+++ pb_ph.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python -B
-import json
 import re
 import requests
+import sqlite3
 
-response = requests.get(
-    'https://www.powerball.com/api/v1/numbers/powerball?_format=json&' +
-        'min=2019-01-01&' +
-        'max=2019-12-31',
-    verify = False,
-    headers = {'Content-Type':'application/json'})
-print
+db = sqlite3.connect('games.db')
+c = db.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='pb'")
+if c.fetchone()[0] == 0:
+    db.execute("CREATE TABLE pb (date TEXT, b1 TEXT, b2 TEXT, b3 TEXT, b4 TEXT, b5 TEXT, b6 TEXT)")
 
-drawings = json.loads(response.text)
-# print(json.dumps(drawings, indent=4))
+for year in range(1997, 2021):
+    for half in [('01-01', '06-30'), ('07-01', '12-31')]:
+        response = requests.get(
+            'https://www.game.com/api/v1/numbers/game?_format=json&' +
+                'min=%s-%s&' % (year, half[0]) +
+                'max=%s-%s' % (year, half[1]),
+            verify = False,
+            headers = {'Content-Type':'application/json'})
+        
+        drawings = response.json()
+        for drawing in drawings:
+            date = drawing['field_draw_date']
+            c = db.execute("SELECT count(date) FROM pb WHERE date='%s'" % date)
+            if c.fetchone()[0] != 0:
+                continue
+        
+            b = re.split(',', drawing['field_winning_numbers'])
+            db.execute("INSERT INTO pb VALUES ('%s', '%02d', '%02d', '%02d', '%02d', '%02d', '%02d')" %
+                           (date, int(b[0]), int(b[1]), int(b[2]), int(b[3]), int(b[4]), int(b[5])))
 
-stats = { }
-for n in range(1, 70):
-    stats['%02d' % n] = 0
-
-for drawing in drawings:
-    numbers = re.split(',', drawing['field_winning_numbers'])
-    for i in range(5):
-        stats[numbers[i]] += 1
-
-print(len(drawings))
-print(json.dumps(stats, indent=4))
+db.commit()
+db.close()
